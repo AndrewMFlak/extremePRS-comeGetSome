7f5f67f13346d61f791367185e8714d97787fed8
AFlak <AndrewMFlak@gmail.com>
2018-05-09 14:33:36 -0400

    updated javascript

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -6,7 +6,37 @@ var userRock = true;
 var userScissors = true;
 var wins = 0;
 var losses = 0;
-var buttonSetup = "$('<input/>').attr({ type: 'button', name:'btn1', value:'am button'})";
+var draws = 0;
+
+function computerSelect() {
+    if (computerChoice < .34) {
+        console.log(computerChoice);
+        console.log("paper");
+        var compPaper = "paper";
+        $(".computerSelection").append('<img id="paper" src="assets/images/paper.jpeg"/>');
+        alert("computer selected paper");
+    } else if (computerChoice <= .67) {
+        console.log(computerChoice);
+        console.log("rock");
+        var compRock = "rock";
+        $(".computerSelection").append('<img id="rock" src="assets/images/rock.jpg"/>');
+        alert("computer selected rock");
+    } else {
+        console.log("scissors");
+        console.log(computerChoice);
+        var compScissors = "scissors";
+        $(".computerSelection").append('<img id="scissors" src="assets/images/scissors.png"/>');
+        alert("computer selected scissors");
+    };
+};
+
+function reset() {
+    $(".button").append('<button class="play">Play Paper Rock Scissors</button>');
+    $(".paperButton").remove();
+    $(".rockButton").remove();
+    $(".scissorsButton").remove();
+};
+
 
 $(".play").click(function () {
     alert("time to Play Paper, Rock, Scissors!!!!");
@@ -21,42 +51,65 @@ $(".play").click(function () {
         alert("The user has selected paper");
         console.log("paper");
         $(".userSelection").append('<img id="paper" src="assets/images/paper.jpeg"/>');
+        computerSelect();
+        if(computerChoice == "paper") {
+            draw();
+        }
+        else if (computerChoice == "scissors") {
+            loss();
+        }
+        else {
+            win();
+        }
     });
     $(".rockButton").click(function () {
         alert("The user has selected rock");
         console.log("rock");
         $(".userSelection").append('<img id="rock" src="assets/images/rock.jpg"/>');
+        computerSelect();
+        if(computerChoice == "paper") {
+            loss();
+        }
+        else if (computerChoice == "scissors") {
+            win();
+        }
+        else {
+            draw();
+        }
     });
     $(".scissorsButton").click(function () {
         alert("The user has selected paper");
         $(".userSelection").append('<img id="scissors" src="assets/images/scissors.png"/>');
+        computerSelect();
+        if(computerChoice == "paper") {
+            win();
+        }
+        else if (computerChoice == "scissors") {
+            draw();
+        }
+        else {
+            loss();
+        }
     });
 });
-$('<input/>').attr({ type: 'button', name: 'btn1', value: 'am button' });
-
-// if (computerChoice < .34) {
-//     console.log("paper");
-//     computerChoice = "paper";
-// } else if (computerChoice <= .67)
-// {
-// console.log("rock");
-// computerChoice = "rock";
-// } else 
-// {
-//     console.log("scissors");
-//     computerChoice = "scissors";
-// };
 
 
 function win() {
     alert("you have defeated your oppenent!!!");
     wins++;
     $(".wins").text(wins);
+    reset();
 };
 
 function loss() {
     alert("you have been defeated by your opponent!!!");
     losses++;
     $(".losses").text(losses);
+    reset();
 };
+
+function draw() {
+    alert("you have drawn with your opponent.  Please play again to see who is the true champion");
+    draws++;
+}
 
