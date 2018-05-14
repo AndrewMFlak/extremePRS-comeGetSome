da728753c135a9ccf9e7f7956221b30f20b62dbd
AFlak <AndrewMFlak@gmail.com>
2018-05-14 14:40:34 -0400

    updated commit of functional js.  pending formatting cleanup.

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,6 +1,6 @@
 console.log("linked");
 
-var computerChoice = Math.floor(Math.random());
+var computerChoice = Math.floor(Math.random() * 1);
 var userPaper = true;
 var userRock = true;
 var userScissors = true;
@@ -10,85 +10,94 @@ var draws = 0;
 
 function computerSelect() {
     if (computerChoice < .34) {
-        console.log(computerChoice);
         console.log("paper");
         var compPaper = "paper";
         $(".computerSelection").append('<img id="paper" src="assets/images/paper.jpeg"/>');
         alert("computer selected paper");
     } else if (computerChoice <= .67) {
-        console.log(computerChoice);
         console.log("rock");
         var compRock = "rock";
         $(".computerSelection").append('<img id="rock" src="assets/images/rock.jpg"/>');
         alert("computer selected rock");
     } else {
         console.log("scissors");
-        console.log(computerChoice);
         var compScissors = "scissors";
         $(".computerSelection").append('<img id="scissors" src="assets/images/scissors.png"/>');
         alert("computer selected scissors");
     };
 };
+function playAgain() {
+    wait(10);
+    alert("Would you like to play again?");
+    $(".play").show();
+    $(".play").click(reset());
 
+}
 function reset() {
-    $(".button").append('<button class="play">Play Paper Rock Scissors</button>');
-    $(".paperButton").remove();
     $(".rockButton").remove();
+    $(".paperButton").remove();
     $(".scissorsButton").remove();
+    $(".userSelection").empty();
+    $(".computerSelection").empty();
+
 };
 
 
 $(".play").click(function () {
-    alert("time to Play Paper, Rock, Scissors!!!!");
-    $(".button").append('<button class="paperButton">Paper</button>');
+    alert("time to play Rock, Paper, Scissors!!!!");
     $(".button").append('<button data-role="button" class="rockButton" data-inline="true" data-mini="true" data-theme="b">Rock</button>');
+    $(".button").append('<button class="paperButton">Paper</button>');
     $(".button").append('<button data-role="button" class="scissorsButton" data-inline="true" data-mini="true" data-theme="b">Scissors</button>');
-    $(".play").remove();
+    $(".play").hide();
     alert("Please select your weapon of choice!!!!");
 
 
     $(".paperButton").click(function () {
         alert("The user has selected paper");
-        console.log("paper");
-        $(".userSelection").append('<img id="paper" src="assets/images/paper.jpeg"/>');
+        console.log("userPaper");
+        console.log(computerChoice);
+        $(".userSelection").append('<img id="paper" height="80%" width="80%" src="assets/images/paper.jpeg"/>');
         computerSelect();
-        if(computerChoice == "paper") {
+        if (computerChoice < .34) {
             draw();
         }
-        else if (computerChoice == "scissors") {
-            loss();
+        else if (computerChoice <= .67) {
+            win();
         }
         else {
-            win();
+            loss();
         }
     });
     $(".rockButton").click(function () {
         alert("The user has selected rock");
-        console.log("rock");
-        $(".userSelection").append('<img id="rock" src="assets/images/rock.jpg"/>');
+        console.log("userRock");
+        console.log(computerChoice);
+        $(".userSelection").append('<img id="rock"  height="80%" width="80%" src="assets/images/rock.jpg"/>');
         computerSelect();
-        if(computerChoice == "paper") {
+        if (computerChoice < .34) {
             loss();
         }
-        else if (computerChoice == "scissors") {
-            win();
+        else if (computerChoice <= .67) {
+            draw();
         }
         else {
-            draw();
+            win();
         }
     });
     $(".scissorsButton").click(function () {
-        alert("The user has selected paper");
-        $(".userSelection").append('<img id="scissors" src="assets/images/scissors.png"/>');
+        alert("The user has selected scissors");
+        console.log("userScissors");
+        console.log(computerChoice);
+        $(".userSelection").append('<img id="scissors" height="80%" width="80%" src="assets/images/scissors.png"/>');
         computerSelect();
-        if(computerChoice == "paper") {
+        if (computerChoice < .34) {
             win();
         }
-        else if (computerChoice == "scissors") {
-            draw();
+        else if (computerChoice <= .67) {
+            loss();
         }
         else {
-            loss();
+            draw();
         }
     });
 });
@@ -97,19 +106,154 @@ $(".play").click(function () {
 function win() {
     alert("you have defeated your oppenent!!!");
     wins++;
-    $(".wins").text(wins);
-    reset();
+    var winText = "Wins: " + wins;
+    $(".wins").text(winText);
+    playAgain();
 };
 
 function loss() {
     alert("you have been defeated by your opponent!!!");
     losses++;
-    $(".losses").text(losses);
-    reset();
+    var lossText = "Loss: " + losses;
+    $(".losses").text(lossText);
+    playAgain();
 };
 
 function draw() {
-    alert("you have drawn with your opponent.  Please play again to see who is the true champion");
+    alert("you have drawn with your opponent.  Please play again to see who is the true champion!!!");
     draws++;
-}
+    var drawText = "Draw: " + draws;
+    $(".draws").text(drawText);
+    playAgain();
+};
 
+
+
+// // fog effect 
+// console.clear();
+
+// canvasWidth = 1600;
+// canvasHeight = 200;
+
+// pCount = 0;
+
+
+// pCollection = new Array();
+
+// var puffs = 1;
+// var particlesPerPuff = 2000;
+// var img = 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/85280/smoke2.png';
+
+// var smokeImage = new Image();
+// smokeImage.src = img;
+
+// for (var i1 = 0; i1 < puffs; i1++) {
+//     var puffDelay = i1 * 1500; //300 ms between puffs
+
+//     for (var i2 = 0; i2 < particlesPerPuff; i2++) {
+//         addNewParticle((i2 * 50) + puffDelay);
+//     }
+// }
+
+
+// draw(new Date().getTime(), 3000)
+
+
+
+// function addNewParticle(delay) {
+
+//     var p = {};
+//     p.top = canvasHeight;
+//     p.left = randBetween(-200, 800);
+
+//     p.start = new Date().getTime() + delay;
+//     p.life = 8000;
+//     p.speedUp = 30;
+
+
+//     p.speedRight = randBetween(0, 20);
+
+//     p.rot = randBetween(-1, 1);
+//     p.red = Math.floor(randBetween(0, 255));
+//     p.blue = Math.floor(randBetween(0, 255));
+//     p.green = Math.floor(randBetween(0, 255));
+
+
+//     p.startOpacity = .3
+//     p.newTop = p.top;
+//     p.newLeft = p.left;
+//     p.size = 200;
+//     p.growth = 10;
+
+//     pCollection[pCount] = p;
+//     pCount++;
+
+
+// }
+
+// function draw(startT, totalT) {
+//     //Timing
+//     var timeDelta = new Date().getTime() - startT;
+//     var stillAlive = false;
+
+//     //Grab and clear the canvas
+//     var c = document.getElementById("myCanvas");
+//     var ctx = c.getContext("2d");
+//     ctx.clearRect(0, 0, c.width, c.height);
+//     c.width = c.width;
+
+//     //Loop through particles
+//     for (var i = 0; i < pCount; i++) {
+//         //Grab the particle
+//         var p = pCollection[i];
+
+//         //Timing
+//         var td = new Date().getTime() - p.start;
+//         var frac = td / p.life
+
+//         if (td > 0) {
+//             if (td <= p.life) { stillAlive = true; }
+
+//             //attributes that change over time
+//             var newTop = p.top - (p.speedUp * (td / 1000));
+//             var newLeft = p.left + (p.speedRight * (td / 1000));
+//             var newOpacity = Math.max(p.startOpacity * (1 - frac), 0);
+
+//             var newSize = p.size + (p.growth * (td / 1000));
+//             p.newTop = newTop;
+//             p.newLeft = newLeft;
+
+//             //Draw!
+//             ctx.fillStyle = 'rgba(150,150,150,' + newOpacity + ')';
+//             ctx.globalAlpha = newOpacity;
+//             ctx.drawImage(smokeImage, newLeft, newTop, newSize, newSize);
+//         }
+//     }
+
+
+
+//     //Repeat if there's still a living particle
+//     if (stillAlive) {
+//         requestAnimationFrame(function () { draw(startT, totalT); });
+//     }
+//     else {
+//         clog(timeDelta + ": stopped");
+//     }
+// }
+
+// function randBetween(n1, n2) {
+//     var r = (Math.random() * (n2 - n1)) + n1;
+//     return r;
+// }
+
+// function randOffset(n, variance) {
+//     //e.g. variance could be 0.1 to go between 0.9 and 1.1
+//     var max = 1 + variance;
+//     var min = 1 - variance;
+//     var r = Math.random() * (max - min) + min;
+//     return n * r;
+// }
+
+// function clog(s) {
+//     console.log(s);
+// }
